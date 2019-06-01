deb60743b56f7b8dd1ab6af39aefb437b5e96f88
Luckas Frigo <luckas@moblee.net>
2019-06-01 00:48:58 -0300

    Connects front and backend

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+
+  callAPI() {
+    fetch("http://localhost:9000/test")
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }));
+  }
+
+  componentWillMount() {
+    this.callAPI();
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +25,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="App-intro">{this.state.apiResponse}</p>
       </div>
     );
   }
