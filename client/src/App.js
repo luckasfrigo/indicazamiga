17307c07d9ef7a9d1c14f87fca6cdaa393ee93d1
Luckas Frigo <luckas@moblee.net>
2019-06-01 15:13:26 -0300

    Creates react docker for deployment

--- client/src/App.js
+++ client/src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to React {process.env.REACT_APP_SERVER_URL}</h2>
         </div>
         <p className="App-intro">{this.state.apiResponse}</p>
       </div>
